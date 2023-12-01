78132998097507657ff7889820cf2f25a39c0cb3
임소영 <soyeong020420@gmail.com>
2023-12-02 02:31:09 +0900

    K-means++의 반환값 NEO Kmeans에 입력

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-from NEOK import *
+from NEOkmeans import *
 from kmeans import *
 import numpy as np
 
 # 군집 수 k
-k = 2
-X = [[1,2,3,4,5],
-     [1,2,3000,10000,100],
-     [1,2,3,4,1],
-     [2,3,4,5,6],
-     [3,4,5,6,7],
-     [4,5,6,7,8],
-     [4,5,5,5,6]]
-
 def cluster(X, k):
     kmeans = Kmeans(k, max_iter=10, tol=1e-4)
     U, C = kmeans.fit(X)
@@ -24,5 +15,3 @@ def cluster(X, k):
     alpha, beta = estimate_alpha_beta(X, C, alpha_delta=1.1, beta_delta=6)
     NeoU, J, J_track = neo_kmeans(X, k, alpha, beta, initU)
     return NeoU
-
-print(cluster(X, k))
